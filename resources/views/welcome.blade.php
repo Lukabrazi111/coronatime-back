d1167df11e832fb9b2b95cb79be57a835c3054f8
Luka Khangoshvili <lukakhangoshvili@gmail.com>
2022-01-05 02:23:55 +0400

    configure .php-cs-fixer
    and install with composer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,23 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="{{ asset("css/app.css") }}">
+    <title>{{ config('app.name') }}</title>
 
-    </head>
-    <body>
-        
-        <script src="{{ asset("js/app.js") }}"></script>
-    </body>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+</head>
+
+<body>
+    <div>
+        <div></div>
+    </div>
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+
 </html>
