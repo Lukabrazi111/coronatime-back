08ef7eeedb9d8beb6b3be2c56556043a2b57f19e
Luka Khangoshvili <lukakhangoshvili@gmail.com>
2022-01-18 17:45:39 +0400

    test: forgot password functionality
    100% coverage

---
+++ tests/Feature/ForgotPasswordTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Http\Livewire\ForgotPassword;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Livewire\Livewire;
+use Tests\TestCase;
+
+class ForgotPasswordTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function test_forgot_password_page_has_livewire_component()
+    {
+        $this->get(route('forgot.password'))
+            ->assertSuccessful()
+            ->assertSeeLivewire('forgot-password');
+    }
+
+    /** @test */
+    public function test_forgot_password_email_required_validation_works_properly()
+    {
+        Livewire::test(ForgotPassword::class)
+            ->set('email', '')
+            ->call('send')
+            ->assertHasErrors(['email' => 'required'])
+            ->assertSee('The email field is required.');
+    }
+
+    /** @test */
+    public function test_forgot_password_properly_email_check()
+    {
+        Livewire::test(ForgotPassword::class)
+            ->set('email', 'dsaxdasx')
+            ->call('send')
+            ->assertHasErrors(['email' => 'email'])
+            ->assertSee('The email must be a valid email address.');
+    }
+
+    /** @test */
+    public function test_forgot_password_page_email_does_not_exist()
+    {
+        $user = User::factory()->create([
+            'name' => 'luka',
+            'email' => 'luka@gmail.com',
+        ]);
+
+        Livewire::actingAs($user)->test(ForgotPassword::class)
+            ->set('email', 'invalidemail@gmail.com')
+            ->call('send')
+            ->assertSessionHas('error_message', 'The selected email is invalid!')
+            ->assertRedirect(route('forgot.password'));
+
+        $this->assertDatabaseHas('users', [
+            'email' => 'luka@gmail.com',
+        ]);
+    }
+
+    /** @test */
+    public function test_forgot_password_page_email_exists()
+    {
+        $user = User::factory()->create([
+            'name' => 'luka',
+            'email' => 'luka@gmail.com',
+        ]);
+
+        Livewire::actingAs($user)->test(ForgotPassword::class)
+            ->set('email', $user->email)
+            ->call('send')
+            ->assertSessionHas('success_message', 'Please check your email to reset password')
+            ->assertRedirect(route('forgot.password'));
+
+        $this->assertDatabaseHas('users', [
+            'email' => 'luka@gmail.com',
+        ]);
+    }
+}
--- tests/Feature/LogoutTest.php
+++ tests/Feature/LogoutTest.php
@@ -10,38 +10,36 @@ use Tests\TestCase;
 
 class LogoutTest extends TestCase
 {
-    use RefreshDatabase;
+	use RefreshDatabase;
 
-    /** @test */
-    public function test_dashboard_has_logout_livewire_component()
-    {
-        $user = User::factory()->create();
-        $this->actingAs($user)->get('/dashboard')
-            ->assertSuccessful()
-            ->assertSeeLivewire('logout');
-    }
+	/** @test */
+	public function test_dashboard_has_logout_livewire_component()
+	{
+		$user = User::factory()->create();
+		$this->actingAs($user)->get('/dashboard')
+			->assertSuccessful()
+			->assertSeeLivewire('logout');
+	}
 
-    /** @test */
-    public function test_can_not_see_livewire_component_if_not_authorized()
-    {
-        $this->get('/dashboard')
-            ->assertDontSeeLivewire('logout')
-            ->assertStatus(302);
-    }
+	/** @test */
+	public function test_can_not_see_livewire_component_if_not_authorized()
+	{
+		$this->get('/dashboard')
+			->assertDontSeeLivewire('logout')
+			->assertStatus(302);
+	}
 
-    /** @test */
-    public function test_user_can_logout_from_dashboard()
-    {
-        $user = User::factory()->create();
+	/** @test */
+	public function test_user_can_logout_from_dashboard()
+	{
+		$user = User::factory()->create();
 
-        $this->actingAs($user)->get('/dashboard')
-            ->assertSuccessful();
+		$this->actingAs($user)->get('/dashboard')
+			->assertSuccessful();
 
-        Livewire::test(Logout::class)
-            ->call('logout')
-            ->assertSessionHas('success_message', 'You are logged out')
-            ->assertRedirect(route('login'));
-    }
-
-
+		Livewire::test(Logout::class)
+			->call('logout')
+			->assertSessionHas('success_message', 'You are logged out')
+			->assertRedirect(route('login'));
+	}
 }
