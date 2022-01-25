6211aa9e56525d13df5f261f91a5c4a3603f40a6
Luka Khangoshvili <lukakhangoshvili@gmail.com>
2022-01-25 20:58:46 +0400

    fix: n+1 problem fetch only critical, recovered, deaths and confirmed

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,22 +8,16 @@ class DashboardController extends Controller
 {
 	public function index()
 	{
-		$countryStatistics = CountryStatistics::all();
-        $newCases = 0;
-        $sumCritical = 0;
-        $sumRecovered = 0;
-        $sumDeaths = 0;
-		foreach ($countryStatistics as $data)
-		{
-			$sumConfirmed = $data->sum('confirmed');
-			$sumCritical = $data->sum('critical');
-			$newCases = $sumConfirmed + $sumCritical;
-			$sumRecovered = $data->sum('recovered');
-			$sumDeaths = $data->sum('deaths');
-		}
+		$countryStatistics = CountryStatistics::select('confirmed', 'critical', 'deaths', 'recovered')->get();
+
+		$sumConfirmed = $countryStatistics->sum('confirmed');
+		$sumCritical = $countryStatistics->sum('critical');
+		$sumCriticalConfirmed = $sumConfirmed + $sumCritical;
+		$sumRecovered = $countryStatistics->sum('recovered');
+		$sumDeaths = $countryStatistics->sum('deaths');
 
 		return view('dashboard', [
-			'confirmed' => number_format($newCases),
+			'confirmed' => number_format($sumCriticalConfirmed),
 			'recovered' => number_format($sumRecovered),
 			'critical'  => number_format($sumCritical),
 			'deaths'    => number_format($sumDeaths),
