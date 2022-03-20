4b6ffd445d18aba64a543a2f3915e40036fdb328
Luka Khangoshvili <lukakhangoshvili@gmail.com>
2022-03-21 01:17:08 +0400

    chore: change url for email verification blade file

--- resources/views/emails/user-registered.blade.php
+++ resources/views/emails/user-registered.blade.php
@@ -8,7 +8,7 @@
 <p style="text-align: center; font-size: 18px; font-style:normal; color:black;">{{ __('click this button to verify your email') }}</p>
 <br>
 <div style="text-align: center;">
-<a href="{{'http://localhost:3000/user/verify/' . $user->verifyUser->token}}" style="color:white; font-weight:900;
+<a href="{{'http://coronatime.lukabrazi.redberryinternship.ge/user/verify/' . $user->verifyUser->token}}" style="color:white; font-weight:900;
 font-size:16px; background-color: #0fba68;
 padding: 15px 80px; border:none;
 border-radius:10px; margin: 0 auto; text-decoration:none; text-transform:uppercase;">{{ __('Verify email') }}</a>
